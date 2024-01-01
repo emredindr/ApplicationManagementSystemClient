c95b52e0bf78db0015f791391b6edb6db8ceaf5b
Emre DİNDAR <75311655+emredindr@users.noreply.github.com>
2024-01-01 15:48:35 +0300

    Update AuthContext
    Localstorage işlemleri güncellendi ve method isimleri güncellendi

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,28 +1,31 @@
-import { createContext,useState } from "react";
+import { createContext,useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("access-token") ? true : false);
 
-  const login = (data) => {
-    console.log(data);
-    // setLoggedIn(true);
-    // setUser(data.result);
-    // localStorage.setItem("access-token", data.result.token);
+  const signIn = (user) => {
+    setLoggedIn(true);
+    setUser(user);
+    localStorage.setItem("access-token", user.token);
   };
+
   const logout = () => {
+    localStorage.removeItem("access-token");
     setLoggedIn(false);
     setUser(null);
-    localStorage.removeItem("access-token");
   };
 
   const values = {
     user,
     loggedIn,
-    login,
+    signIn,
     logout,
   };
+  
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
+
+export const useAuth = () => useContext(AuthContext);
