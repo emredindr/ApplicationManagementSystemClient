d79ea3a6b84d3beb3075429129655deefcf46b05
Emre DİNDAR <75311655+emredindr@users.noreply.github.com>
2024-01-01 15:58:23 +0300

    Admin Dashboard Page
    login olan admin bu sayfaya yönlendirilir. sayfada başburuların oranlarını görebilir.

---
+++ src/pages/admin/admin-dashboard/AdminDashboard.jsx
@@ -0,0 +1,40 @@
+import ApplicationStatusChart from "./ApplicationStatusChart";
+import { useEffect, useState } from "react";
+import { getApplicationStatusRatio } from "../../../services/api";
+import withLoading from "../../../hoc/withLoading";
+import Loading from "../../../components/Loading";
+
+const AdminDashboard = ({ loading, setLoading }) => {
+  const [applicationStatusData, setApplicationStatusData] = useState({});
+
+  const getApplicationStatusData = async () => {
+    try {
+      setLoading(true);
+      const response = await getApplicationStatusRatio();
+      console.log(response);
+      setApplicationStatusData(response.result);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getApplicationStatusData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="admin-dashboard-container">
+      <div className="admin-dashboard-chart-container">
+        <ApplicationStatusChart data={applicationStatusData} />
+      </div>
+    </div>
+  );
+};
+
+export default withLoading(AdminDashboard);
